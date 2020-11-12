89e844ef47757c02b4c1c20bb6b44c553bcfbc31
Abdelrahman mysara <bodimysara@yahoo.com>
2020-11-12 11:44:12 +0200

    Add files via upload

---
+++ flask_app/api.py
@@ -0,0 +1,42 @@
+from graphene import ObjectType, Field, ID, Schema, String, List, Int
+from datetime import datetime
+from sqlalchemy.orm import sessionmaker
+from flask_app.models import Scraped, db_connect
+from graphene_sqlalchemy import SQLAlchemyObjectType
+
+
+engine = db_connect()
+Session = sessionmaker(autocommit=False,
+                       autoflush=False,
+                       bind=engine)
+session = Session()
+
+
+"""
+this is a graphene_sqlalchemy class called SQLAlchemyObjectType 
+this allows you to have the graphql object of your model without having to 
+define it if you want to know more about graphene_sqlalchemy read here
+https://github.com/graphql-python/graphene-sqlalchemy
+"""
+class Product(SQLAlchemyObjectType):
+    class Meta:
+        model = Scraped
+
+"""
+this is the object type that is used for defining the query for graphql , here allProducts property is resolved using the 
+resolve_allProducts and the same for the product property 
+"""
+class Query(ObjectType):
+    allProducts = List(Product)
+    product = Field(Product, id=ID())
+
+    def resolve_product(self, args, context, info):
+        query = session.query(Scraped).filter(Scraped.id == args['id'])
+        return query.first()
+
+    def resolve_allProducts(self, args, context, info):
+        query = session.query(Scraped).order_by(Scraped.id)  # SQLAlchemy query
+        return query.all()
+
+
+schema = Schema(query=Query)
---
+++ flask_app/models.py
@@ -0,0 +1,36 @@
+from scrapy.utils.project import get_project_settings
+from sqlalchemy import (
+    Integer, String, Date, DateTime, Float, Boolean, Text)
+from sqlalchemy import create_engine, Column, Table, ForeignKey, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+def db_connect():
+    """
+    this function returns the configuration of the database using the CONNECTION_STRING
+    the CONNECTION_STRING is defined in the settings.py file
+    """
+    return create_engine(get_project_settings().get("CONNECTION_STRING"))
+
+
+def create_table(engine):
+    Base.metadata.create_all(engine)
+
+"""
+    this is the table that is created in the database
+"""
+class Scraped(Base):
+    __tablename__ = 'scraped'
+
+    id = Column(Integer, primary_key=True)
+    image_urls = Column('image_urls', Text())
+    link = Column('link', Text())
+    title = Column('title', Text())
+    rating = Column('rating', Text())
+    no_of_reviews = Column('no_of_reviews', Text())
+    description = Column('description', Text())
+    comments = Column('comments', Text())
+    rating_distribution = Column('rating_distribution', Text())
+    features = Column('features', Text())
